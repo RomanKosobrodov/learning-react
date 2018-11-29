93e8390589b77d839f5ecfb5ae8bed8ee3404814
Roman <kosobrodov@fastmail.fm>
2018-11-29 15:21:11 +1100

    added Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Menu, 
+    Container,
+Segment } from 'semantic-ui-react';
+
+
+const FooterComponent = () =>
+    <Segment inverted>
+        <Container centre aligned>
+            <p>Footer </p>        
+        </Container>
+    </Segment>
+
+export default FooterComponent;
